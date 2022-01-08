6244bd2f974b153e0ffec17114b132170e5e863b
ajo <ajo.antu@gmail.com>
2022-01-09 00:16:01 +0530

    navbar ongoing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Banner from "./Containers/Banner";
+import Navbar from "./Containers/navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Banner />
     </div>
   );
--- src/Containers/Banner.js
+++ src/Containers/Banner.js
@@ -7,22 +7,25 @@ function Banner() {
   const [car, setCar] = useState([]);
   useEffect(() => {
     $.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apikey}`
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`
     ).then((res) => {
       setCar(res.articles);
       console.log(res.articles);
     });
   }, []);
   return (
-    <div>
+    <div className="row  justify-content-center">
       {car.map(({ urlToImage, title, description, url, source }) => (
-        <div className="card-xl-8" style={{ width: "18rem" }}>
+        <div
+          className="card-md-8 "
+          style={{ width: "18rem", height: "30rem", borderColor: "black" }}
+        >
           <img src={urlToImage} class="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title ? title : ""}</h5>
-            <p className="card-text">{description ? description : ""}</p>
+            <p className="card-text ">{description ? description : ""}</p>
+            <a>{source.name}</a>
             <a href={url} className="btn btn-primary">
-              <a>{source.name}</a>
               further details
             </a>
           </div>
@@ -35,3 +38,4 @@ export default Banner;
 //API  KET ::2bca595428e88c180cb42f9b6f2506cf
 //https://newsapi.org/v2/everything?q=bitcoin&apiKey=2bca59506cf5428e88c180cb42f9b6f2
 //https://newsapi.org/v2/top-headlines?country=us&apiKey=2bca59506cf5428e88c180cb42f9b6f2
+//https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2bca59506cf5428e88c180cb42f9b6f2
